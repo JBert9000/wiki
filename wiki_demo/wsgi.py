5e7744ec8fa73beb6d1216fe68c4003c33e8e99e
John Bert <jbertlik@gmail.com>
2020-06-10 11:23:15 -0400

    Got rid of a line in the wsgi.py file

--- wiki_demo/wsgi.py
+++ wiki_demo/wsgi.py
@@ -12,8 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 # from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki_demo.settings.production')
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki_demo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki_demo.settings')
 
 
 application = get_wsgi_application()
