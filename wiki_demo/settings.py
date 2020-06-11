41251ba6267bbe9fec4ff201a1376b617b37befc
John Bert <jbertlik@gmail.com>
2020-06-11 09:06:27 -0400

    Turned debug mode on to understand what the problem is

--- wiki_demo/settings.py
+++ wiki_demo/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('WIKI_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DEBUG_VALUE')
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['myproject.herokuapp.com', 'localhost']
 
 
