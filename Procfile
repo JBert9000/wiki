797baae17ec17888e8299a989b8f81dd56bc4229
John Bert <jbertlik@gmail.com>
2020-06-11 08:49:25 -0400

    Took out whitenoise from wsgi.py file and took out --log-file - from the Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wiki_demo.wsgi --log-file -
+web: gunicorn wiki_demo.wsgi
--- wiki_demo/wsgi.py
+++ wiki_demo/wsgi.py
@@ -10,10 +10,12 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki_demo.settings')
 
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
+
+# web: gunicorn wiki_demo.wsgi --log-file -
