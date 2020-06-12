64cc9d020b6ec8e9c1eb0da28c54fe52a6870210
John Bert <jbertlik@gmail.com>
2020-06-12 06:39:29 -0400

    Turned django-heroku back on.

--- wiki_demo/settings.py
+++ wiki_demo/settings.py
@@ -1,5 +1,5 @@
 import os
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,7 +145,7 @@ STATICFILES_DIRS = (
 #     '/wiki_demo/wiki_demo/wiki_pics',
 # ]
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 
 LOGGING = {
