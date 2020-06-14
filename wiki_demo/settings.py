968258aa511fd6f1d3c26fb0d322259fd8554db5
John Bert <jbertlik@gmail.com>
2020-06-14 08:07:56 -0400

    Added the AWS_S3_REGION_NAME variable in settings.py

--- wiki_demo/settings.py
+++ wiki_demo/settings.py
@@ -150,6 +150,8 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_S3_REGION_NAME = "ap-south-1"
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
