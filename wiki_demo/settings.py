900ed12226090d2ca1a45db8b3345544f11a488a
John Bert <jbertlik@gmail.com>
2020-06-14 08:11:07 -0400

    Changed the variable AWS_S3_REGION_NAME to us-east-2

--- wiki_demo/settings.py
+++ wiki_demo/settings.py
@@ -150,7 +150,7 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_REGION_NAME = "us-east-2"
 
 LOGGING = {
     'version': 1,
