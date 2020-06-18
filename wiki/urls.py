c43f8c90e0385218d34aa57efe0e5aad6be11d80
John Bert <jbertlik@gmail.com>
2020-06-18 10:53:43 -0400

    Added pagination and brought back delete functionality.

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,24 +1,23 @@
 from django.urls import path
 from .views import (
+    WikiListView,
     WikiDetailView,
     WikiCreateView,
     WikiUpdateView,
-    # WikiDeleteView,
+    WikiDeleteView,
     WikiEditsView,
     WikiResultsView,
-    dummy_view,
-    dummy_list_view,
 )
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name="wiki-home"),
+    path('', WikiListView.as_view(), name="wiki-home"),
     path('s/', views.search, name="wiki-search"),
     path('results/', WikiResultsView.as_view(), name="wiki-results"),
     path('content/new/', WikiCreateView.as_view(), name="wiki-create"),
     path('content/<int:pk>/update/', WikiUpdateView.as_view(), name="wiki-update"),
-    # path('content/<int:pk>/delete/', WikiDeleteView.as_view(), name="wiki-delete"),
+    path('content/<int:pk>/delete/', WikiDeleteView.as_view(), name="wiki-delete"),
     path('content/<int:pk>/', WikiDetailView.as_view(), name="wiki-details"),
     path('content/<int:pk>/edits/', WikiEditsView.as_view(), name="wiki-edits"),
     path('dummy/', views.dummy_list_view, name="dummy-list"),
--- wiki/views.py
+++ wiki/views.py
@@ -4,7 +4,7 @@ from django.views.generic import (
     DetailView,
     CreateView,
     UpdateView,
-    # DeleteView,
+    DeleteView,
 )
 from .models import Content
 from django.urls import reverse
@@ -16,34 +16,34 @@ dummy_data = {
     1: {
         'id': 1,
         'title': 'Fox',
-        'content': 'This is dummy data that is not editable in this demo, but anyone is encouraged to create wiki posts which can also be edited by anyone. Typically a user can create, edit, and delete posts without making a profile. Links via the sidebar will automaically be made based on the title of each wiki post. Go back to the home page to check out more posts.',
-        'date_posted': 'June 15th 2020',
-        'date_updated': 'June 16th 2020',
-        'history': {'June 15th 2020', 'June 16th 2020'},
+        'content': 'This is dummy data that is not editable in this demo, but anyone is encouraged to create wiki posts which can also be edited by anyone. Typically a user can create, edit, and delete posts without making a profile. Links via the sidebar will automaically be made based on the title of each wiki post.',
+        'date_posted': 'June 10th 2020',
+        'date_updated': 'June 11th 2020',
+        'history': {'June 10th 2020', 'June 11th 2020'},
     },
     2: {
         'id': 2,
         'title': 'Frog',
-        'content': 'This is a picture of a frog.',
-        'date_posted': 'June 15th 2020',
-        'date_updated': 'June 16th 2020',
-        'history': {'June 15th 2020', 'June 16th 2020'},
+        'content': 'While this data is static and is populated from seeded JSON data and pictures from the Django static folder, the wiki posts that users can upload will be stored in a Postgres database within the Django / Heroku backend. All pictures that are uploaded to this site are stored via AWS S3.',
+        'date_posted': 'June 11th 2020',
+        'date_updated': 'June 12th 2020',
+        'history': {'June 11th 2020', 'June 12th 2020'},
     },
     3: {
         'id': 3,
         'title': 'Cat',
-        'content': 'This is a picture of a cat.',
-        'date_posted': 'June 15th 2020',
-        'date_updated': 'June 16th 2020',
-        'history': {'June 15th 2020', 'June 16th 2020'},
+        'content': 'There is search functionality built into this site which searches the title of the Content model to see if it matches any critera. Go ahead and try it out.',
+        'date_posted': 'June 12th 2020',
+        'date_updated': 'June 13th 2020',
+        'history': {'June 12th 2020', 'June 13th 2020'},
     },
     4: {
         'id': 4,
         'title': 'Russian Blue Cat',
-        'content': 'This is a picture of a Russian Blue cat.',
-        'date_posted': 'June 15th 2020',
-        'date_updated': 'June 16th 2020',
-        'history': {'June 15th 2020', 'June 16th 2020'},
+        'content': 'I have added a module for Django called summernote which allows markdown text and other features when editing wiki posts. I thought this would be more interesting to incorporate.',
+        'date_posted': 'June 13th 2020',
+        'date_updated': 'June 14th 2020',
+        'history': {'June 13th 2020', 'June 14th 2020'},
     },
 }
 
@@ -79,43 +79,21 @@ def home(request):
     return render(request, 'wiki/home.html', context)
 
 
+class WikiListView(ListView):
+    model = Content
+    template_name = 'wiki/home.html'
+    context_object_name = 'contents'
+    ordering = ['-date_updated']
+    paginate_by = 5
+
+
 class WikiResultsView(ListView):
     model = Content
     template_name = 'wiki/results.html'
 
 
-class WikiListView(ListView):
-    model = Content
-    template_name = 'wiki/home.html'
-    ordering = ['-date_updated']
-
-
 class WikiDetailView(DetailView):
     model = Content
-
-
-def dummy_list_view(request):
-    context = {
-        'dummy_data': dummy_data,
-        }
-
-    return render(request, "wiki/dummy_data_list.html", context)
-
-
-def dummy_view(request, id):
-    context = {
-        'dummy_data': dummy_data[id],
-        }
-
-    return render(request, "wiki/dummy_data.html", context)
-
-
-def dummy_edit_history(request, id):
-    context = {
-        'dummy_data': dummy_data[id],
-        }
-
-    return render(request, "wiki/dummy_edits.html", context)
 
 
 class WikiCreateView(CreateView):
@@ -140,11 +118,12 @@ class WikiUpdateView(UpdateView):
         return reverse("wiki-details", kwargs={"pk": self.object.id})
 
 
-# class WikiDeleteView(DeleteView):
-#     model = Content
-#
-#     def get_success_url(self):
-#         return reverse("wiki-home")
+class WikiDeleteView(DeleteView):
+    model = Content
+    template_name = 'wiki/content_delete.html'
+
+    def get_success_url(self):
+        return reverse("wiki-home")
 
 
 class WikiEditsView(DetailView):
@@ -153,3 +132,27 @@ class WikiEditsView(DetailView):
 
     def get_success_url(self):
         return reverse("wiki-details", kwargs={"pk": self.object.id})
+
+
+def dummy_list_view(request):
+    context = {
+        'dummy_data': dummy_data,
+        }
+
+    return render(request, "wiki/dummy_data_list.html", context)
+
+
+def dummy_view(request, id):
+    context = {
+        'dummy_data': dummy_data[id],
+        }
+
+    return render(request, "wiki/dummy_data.html", context)
+
+
+def dummy_edit_history(request, id):
+    context = {
+        'dummy_data': dummy_data[id],
+        }
+
+    return render(request, "wiki/dummy_edits.html", context)
