8f4861cacae64b7bb5a9866dee09a89d83f77632
John Bert <jbertlik@gmail.com>
2020-06-19 11:25:03 -0400

    Reconfigured the settings.py file

--- wiki_demo/settings.py
+++ wiki_demo/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,28 +121,36 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+"""
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
+# STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # FORCE_SCRIPT_NAME = '/wiki'
 # STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 STATIC_URL = '/static/'
 
+
+
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
+"""
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+"""
 # Additional locations of static files
 STATICFILES_DIRS = (
     os.path.join('static'),
 )
+"""
 
-django_heroku.settings(locals())
+STATICFILES_DIRS = ['static'] # This is relative to the project_root
+
+
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -193,3 +201,6 @@ LOGGING = {
         },
     }
 }
+
+
+django_heroku.settings(locals())
